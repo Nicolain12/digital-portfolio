d641e42c886878da83f094bc6c839b1c22fa4caa
Nicolain12 <lainnicolas1@gmail.com>
2023-08-15 18:59:19 -0300

    knowledge done, certificates on process

--- src/controller/indexController.js
+++ src/controller/indexController.js
@@ -1,5 +1,15 @@
+const fs = require('fs');
+
 module.exports = {
     index: function (req, res, next) {
-        res.render('index', { title: 'Portfolio' });
+        const knowledge = JSON.parse(fs.readFileSync(__dirname + '/../database/technologies.json', 'utf8'));
+        const certificates = JSON.parse(fs.readFileSync(__dirname + '/../database/certificates.json', 'utf8'));
+        res.render('index', {
+            title: 'Portfolio', 
+            backEnd: knowledge.back,
+            frontEnd: knowledge.front,
+            tools: knowledge.tools,
+            certificates: certificates
+        });
     },
-}
+};
